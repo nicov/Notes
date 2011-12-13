a5467a2d4fbef0fe67d845bc5cdd3343423e8052
nicolas vetel <nvetel.ext@orange-ftgroup.com>
2011-12-13 11:53:27 +0100

    Setting of nginx support in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,10 +10,10 @@ Notes::Application.configure do
   config.action_controller.perform_caching = true
 
   # Specifies the header that your server uses for sending files
-  config.action_dispatch.x_sendfile_header = "X-Sendfile"
+  # config.action_dispatch.x_sendfile_header = "X-Sendfile"
 
   # For nginx:
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
 
   # If you have no front-end server that supports something like X-Sendfile,
   # just comment this out and Rails will serve the files
@@ -29,7 +29,7 @@ Notes::Application.configure do
 
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
-  config.serve_static_assets = true
+  config.serve_static_assets = false
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
@@ -48,13 +48,13 @@ Notes::Application.configure do
   config.active_support.deprecation = :notify
   
   # Compress JavaScript and CSS  
-  config.assets.compress = false  
+  config.assets.compress = true  
        
   # Don't fallback to assets pipeline  
   config.assets.compile = true  
        
   # Generate digests for assets URLs  
-  config.assets.digest = false
+  config.assets.digest = true
 
   # Ajoute pour devise mais visiblement facultatif
 #  config.action_mailer.default_url_options = { :host => 'notes' }
--- features/user_searches_note.feature
+++ features/user_searches_note.feature
@@ -8,10 +8,9 @@ Feature: Notes search
     And the note "note1" with content "This is a note"
     And the note "memo1" with content "This is a memo"
     When I go to the home page
-    Then I should see "Search"
     And I should see "note1"
     And I should see "memo1"
-    When I fill in "Search for:" with "memo"
+    When I fill in "search" with "memo"
     And I press "Search"
     Then I should see "memo1"
     And I should not see "note1"
